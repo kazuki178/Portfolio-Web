eb1c878417fda98a4b9f31ca6f01b9b329d70694
kazuki178 <yahashi.kazuki0521@icloud.com>
2024-02-19 00:38:38 +0900

    animation削除

--- scroll.js
+++ scroll.js
@@ -1,27 +1,27 @@
-ScrollReveal({
-    reset: false,
-    distance: "600px",
-    duration: 1100, //2sかけてアニメーション
-    delay: 200 /* 0.2s後に発動 */,
-});
-ScrollReveal().reveal(".top", {
-    delay: 200,
-    origin: "top",
-});
+// ScrollReveal({
+//     reset: false,
+//     distance: "600px",
+//     duration: 1100, //2sかけてアニメーション
+//     delay: 200 /* 0.2s後に発動 */,
+// });
+// ScrollReveal().reveal(".top", {
+//     delay: 200,
+//     origin: "right",
+// });
 
 
-ScrollReveal().reveal(".icons", {
-    delay: 200,
-    origin: "left",
-});
+// ScrollReveal().reveal(".icons", {
+//     delay: 200,
+//     origin: "left",
+// });
 
-ScrollReveal().reveal(".name", {
-    delay: 200,
-    origin: "right",
-});
+// ScrollReveal().reveal(".name", {
+//     delay: 200,
+//     origin: "right",
+// });
 
 
-ScrollReveal().reveal(".mian-contents,.News-tables", {
-    delay: 200,
-    origin: "bottom",
-});
+// ScrollReveal().reveal(".mian-contents", {
+//     delay: 0,
+//     origin: "left",
+// });
